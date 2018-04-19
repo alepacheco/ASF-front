b433ed1b0886806810201b313404f72bb22e58aa
Alejandro Pacheco <alepacheco99@gmail.com>
2018-04-19 17:31:34 +0200

    Fixed autolocate

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 var path = require('path');
 var express = require('express');
 var app = express();
+var request = require("request");
 
 app.listen(80);
 app.use(express.static('public'));
@@ -16,9 +17,24 @@ app.get('/geo', async function(req, res, next) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const response = await fetch('http://ip-api.com/json/' + ip);
   const jsonResponse = await response.json()
-  res.send(await jsonResponse.city);
+
+  getIata(await jsonResponse.city, res.send);
 });
 
+
+
+const getIata = (cityname, callback) => {
+  var options = { method: 'GET',
+    url: 'https://clients1.google.com/complete/search',
+    qs: { client: 'flights', q: cityname },
+    headers: { 'cache-control': 'no-cache' }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    callback(JSON.parse(body.substr(19,body.length - 20))[1][0][3]['a'])
+  });
+}
 
 
 app.get('/', async function(req, res, next) {
