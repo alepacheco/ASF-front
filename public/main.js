c0396e289f5835bb60b87d1f2a6dcf810b94bec6
Alejandro Pacheco <alepacheco99@gmail.com>
2018-04-19 15:52:22 +0200

    Change styles

--- public/main.js
+++ public/main.js
@@ -1,6 +1,56 @@
-'use stric';
+
+// function(response) {
+//   $("#results").text(`Searching flights from: ${response.departure} to: ${response.destination} on: ${response.departure_date} at: ${response.departure_time}` );
+//   if (response.departure && response.destination && response.departure_date && response.return_date) {
+//     $("#results").text('Return trip');
+//     const edreamsUrl = `https://www.edreams.com/#/results/type=R;dep=${response.departure_date};from=${response.departure};to=${response.destination};ret=${response.return_date}`
+//     window.location.replace(edreamsUrl);
+//   }
+//   else if (response.departure && response.destination && response.departure_date ) {
+//     $("#results").text('OW trip');
+//     const edreamsUrl = `https://www.edreams.com/#/results/type=O;dep=${response.departure_date};from=${response.departure};to=${response.destination}`;
+//     window.location.replace(edreamsUrl);
+//   }
+//   else if (response.destination && response.departure_date) {
+//     $("#results").text('Geolocating');
+//     $.ajax({url: "/geo", success: function(departure){
+//       const edreamsUrl = `https://www.edreams.com/#/results/type=O;dep=${response.departureDate};from=${departure};to=${response.destination}`;
+//       //window.location.replace(edreamsUrl);
+//     }});
+//   }
+// }
+
+function evaluateText() {
+  searchApi(function(response) {
+    $("#results").text(JSON.stringify(response));
+  })
+}
+
+async function makeUrl(params) {
+  if (!params.departure) {
+    params.departure = await $.ajax({url: "/geo"});
+  }
+  if (params.departure_date && params.return_date && params.destination) {
+    return `https://www.edreams.com/#/results/type=R;dep=${params.departure_date};from=${params.departure};to=${params.destination};ret=${params.return_date}`
+  } else if (params.departure_date && params.destination) {
+    return `https://www.edreams.com/#/results/type=O;dep=${params.departure_date};from=${params.departure};to=${params.destination}`
+  }
+}
 
 function search() {
+  searchApi(async function(response) {
+    const url = await makeUrl(response);
+    openInNewTab(url);
+  })
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+
+function searchApi(callback) {
   $.ajax({
     url: "/parse",
     type: "get",
@@ -8,26 +58,16 @@ function search() {
       q: $('#inputField').val()
     },
     success: function(response) {
-      $("#results").text(`Searching flights from: ${response.departure} to: ${response.destination} on: ${response.departure_date} at: ${response.departure_time}` );
-      if (response.departure && response.destination && response.departure_date) {
-        $("#results").text('Redirecting');
-        const edreamsUrl = `https://www.edreams.com/#/results/type=O;dep=${response.departure_date};from=${response.departure};to=${response.destination}`;
-        window.location.replace(edreamsUrl);
-      }
-      else if (response.destination && response.departure_date) {
-        $("#results").text('Geolocating');
-        $.ajax({url: "/geo", success: function(departure){
-          const edreamsUrl = `https://www.edreams.com/#/results/type=O;dep=${response.departureDate};from=${departure};to=${response.destination}`;
-          window.location.replace(edreamsUrl);
-        }});
-      }
+      callback(response);
     },
     error: function(xhr) {
       //Do Something to handle error
     }
   });
-
 }
+
+
+
 
 function startDictation() {
   const showListeningOverlay = function() {
